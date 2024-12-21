9904c9f5fa8f115768f38bec4b1e89d751c8af88
Vivian <antdragone+pulsagit@gmail.com>
2024-12-21 00:02:02 -0500

    Update flist-music.js

--- flist-music.js
+++ flist-music.js
@@ -1,52 +1,84 @@
 // ==UserScript==
-// @name         Replace Text with iFrame
-// @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  Replace specific text with an iframe from a custom tag
-// @author       Your Name
-// @match        *://*/*
+// @name         Media Embedding
+// @namespace    https://github.com/pulsafermi/flist-music/edit/main/flist-music.js
+// @version      1.0
+// @description  Opens a borderless popup for media playlists in F-List BBCode
+// @match        *://*.f-list.net/c/*
 // @grant        none
 // ==/UserScript==
+
 // Instructions
 /* 
-This is a custom script that does a very simple thing: it replaces custom tags with an iframe.
+This is a custom script that does a very simple thing: it replaces custom tags with a popup.
 
 The custom BBCode tag it's looking for is [spotify] - this isn't a tag that is normally parsed by BBCode.
 
-Simply enter your Spotify playlist like; [spotify]https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M[/spotify] and it will be replaced with an iframe.
+Simply enter your Spotify playlist like; [spotify]https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M[/spotify] and it will be replaced with a popup.
 */
 /*
 Profile Template:
-[center]
-[collapse=Spotify Playlist]
 [spotify]URL[/spotify]
-Hi! If you see this message, it means that you don't have the tampermonkey script installed.
-Tampermonkey is a browser extension that allows you to install custom scripts to change the way a web page behaves.
-You can install the plugin by clicking [url=https://www.tampermonkey.net/]here[/url] - please note, that this plugin can be used nefariously.
-Don't install scripts from untrusted sources, and always read the code before installing.
-You can find the scripts that I write [url=https://github.com/pulsafermi/flist-scripts]here[/url].
-[/collapse]
-[/center]
 */
 
 (function() {
     'use strict';
 
-    // Function to replace custom [spotify]URL[/spotify] tags with iframe
-    function replaceSpotifyTags() {
+    function replaceMediaTags() {
         const bodyText = document.body.innerHTML;
-        const spotifyTagRegex = /\[center\]\s*\[collapse=Spotify Playlist\]\s*\[spotify\](.*?)\[\/spotify\][\s\S]*?\[\/collapse\]\s*\[\/center\]/g;
-        const newBodyText = bodyText.replace(spotifyTagRegex, (match, p1) => {
-            if (p1.startsWith("https://open.spotify.com/")) {
-                const iframe = `<iframe style="border-radius:12px" src="${p1}" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
-                return iframe;
-            } else {
-                return `[color=red]This Profile tried to load an invalid Spotify URL, or tried to inject a different URL into the iframe. The URL is ${p1}[/color]`;
-            }
-        });
+        const spotifyTagRegex = /\[spotify\](.*?)\[\/spotify\]/g;
+        const youtubeTagRegex = /\[ytvid\](.*?)\[\/ytvid\]/g;
+        const youtubePlaylistTagRegex = /\[ytplaylist\](.*?)\[\/ytplaylist\]/g;
+
+        const newBodyText = bodyText
+            .replace(spotifyTagRegex, (match, p1) => {
+                const uniqueCodeMatch = p1.match(/https:\/\/open\.spotify\.com\/playlist\/([a-zA-Z0-9]+)/);
+                if (uniqueCodeMatch) {
+                    const uniqueCode = uniqueCodeMatch[1];
+                    const iframe = `
+                    <button onclick="openPopup('https://open.spotify.com/embed/playlist/${uniqueCode}?utm_source=generator&theme=0')" style="border:none; background:none; cursor:pointer; color:cyan; text-decoration:underline;">Open Spotify Playlist</button>`;
+                    return iframe;
+                } else {
+                    return `<p style="color:red;">This Profile tried to load an invalid Spotify URL. The URL is ${p1}</p>`;
+                }
+            })
+            .replace(youtubeTagRegex, (match, p1) => {
+                const videoIdMatch = p1.match(/https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/);
+                if (videoIdMatch) {
+                    const videoId = videoIdMatch[1];
+                    const iframe = `
+                    <button onclick="openPopup('https://www.youtube.com/embed/${videoId}')" style="border:none; background:none; cursor:pointer; color:cyan; text-decoration:underline;">Open Youtube Video</button>`;
+                    return iframe;
+                } else {
+                    return `<p style="color:red;">This Profile tried to load an invalid YouTube Video URL. The URL is ${p1}</p>`;
+                }
+            })
+            .replace(youtubePlaylistTagRegex, (match, p1) => {
+                const playlistIDMatch = p1.match(/https:\/\/www\.youtube\.com\/playlist\?list=([a-zA-Z0-9_-]+)&?/);
+                if (playlistIDMatch) {
+                    const playlistId = playlistIDMatch[1];
+                    const iframe = `
+                    <button onclick="openPopup('https://www.youtube.com/embed/videoseries?si=lEeT0ovqHmUVmXh4&amp;list=${playlistId}')" style="border:none; background:none; cursor:pointer; color:cyan; text-decoration:underline;">Open Youtube Playlist</button>`;
+                    return iframe;
+                } else {
+                    return `<p style="color:red;">This Profile tried to load an invalid YouTube Playlist URL. The URL is ${p1}</p>`;
+                }
+            });
+
         document.body.innerHTML = newBodyText;
     }
 
+    // Function to open a new window with the Spotify URL
+    window.openPopup = function(url) {
+        const width = 800;
+        const height = 600;
+        const left = (window.screen.width - width) / 2;
+        const top = (window.screen.height - height) / 2;
+        const options = `width=${width},height=${height},left=${left},top=${top},scrollbars=yes`;
+        window.open(url, '_blank', options);
+    };
+
     // Run the function after the page loads
-    window.addEventListener('load', replaceSpotifyTags);
+    window.addEventListener('load', replaceMediaTags);
 })();
+
+
